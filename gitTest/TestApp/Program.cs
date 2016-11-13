b5dfa3839d5937cb72ab247cb5a04fc1dab268ec
rabbit66376 <rabbit66376@gmail.com>
2016-11-13 17:55:38 +0800

    use new  feature in app

--- gitTest/TestApp/Program.cs
+++ gitTest/TestApp/Program.cs
@@ -11,6 +11,11 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("OK I got a new move");
+            var data = new feature.data();
+            data.name = "Johnson";
+
+            Console.WriteLine(data.name);
+            Console.Read();
         }
     }
 }
